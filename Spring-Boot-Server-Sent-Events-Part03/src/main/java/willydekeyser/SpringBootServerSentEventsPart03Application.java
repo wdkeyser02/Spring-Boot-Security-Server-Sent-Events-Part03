0b57a617eb6b75c4f38834acd6f071008308d26d
Willy De Keyser <spring.boot.programming@gmail.com>
2026-09-11 09:21:16 +0200

    Setup

---
+++ Spring-Boot-Server-Sent-Events-Part03/src/main/java/willydekeyser/SpringBootServerSentEventsPart03Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerSentEventsPart03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerSentEventsPart03Application.class, args);
+	}
+
+}
